ea7cd4f3660baee10c74fcad3cdd8dd7489f42d4
amnerysdev <amnerysdev@gmail.com>
2026-02-21 11:39:06 -0400

    moving the ship through keyboard input

--- code/main.py
+++ code/main.py
@@ -17,7 +17,7 @@ coordinates = [(random.randint(0,WINDOW_WIDTH),random.randint(0,WINDOW_HEIGHT)) 
 
 player_surf = pygame.image.load("assets/images/player.png").convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-player_movement = pygame.math.Vector2(1,1)
+player_direction = pygame.math.Vector2(0,0)
 player_speed = 100 
 
 meteor_surf = pygame.image.load("assets/images/meteor.png").convert_alpha()
@@ -33,22 +33,20 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
              running = False
-             
+    
+    #player movement and input
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_rect.center +=  player_direction * player_speed * dt
+         
     #drawing the game :)
     display_surface.fill("darkgrey")
     for coordinate in coordinates:
         display_surface.blit(star_surf, coordinate)
         
     display_surface.blit(meteor_surf, meteor_rect)
-    display_surface.blit(laser_surf, laser_rect)
-    
-    #player movement
-    player_rect.center+= player_speed * player_movement * dt
-    if player_rect.bottom > WINDOW_HEIGHT or player_rect.y < 0:
-        player_movement.y *= -1
-    if player_rect.right > WINDOW_WIDTH or player_rect.left < 0:
-        player_movement.x *= -1
-        
+    display_surface.blit(laser_surf, laser_rect)  
     display_surface.blit(player_surf, player_rect)
     
     
